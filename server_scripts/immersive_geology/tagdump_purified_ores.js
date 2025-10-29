b770ef9006adf954c202a4a3cf6c30d1dc0218ca
calmezyy <322.salmo@gmail.com>
2025-10-29 17:20:20 +0000

    Added GS recipes for GTCEU ORES

---
+++ server_scripts/immersive_geology/tagdump_purified_ores.js
@@ -0,0 +1,16 @@
+// KubeJS 6, Rhino-safe, no Java interop
+ServerEvents.tags('item', function (event) {
+  var tag = event.get('forge:purified_ores');
+  if (!tag) {
+    console.info('[TAG DUMP] forge:purified_ores not found');
+    return;
+  }
+
+  var ids = tag.getObjectIds().toArray(); // Java array
+  var out = [];
+  for (var i = 0; i < ids.length; i++) out.push(String(ids[i])); // to JS strings
+  out.sort();
+
+  console.info('[TAG DUMP] forge:purified_ores entries=' + out.length);
+  for (var j = 0; j < out.length; j++) console.info(' - ' + out[j]);
+});
