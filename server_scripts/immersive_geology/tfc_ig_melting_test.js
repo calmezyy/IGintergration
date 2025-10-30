4c54fcd2e65ad98128f7342cd1d14fedf72985bd
calmezyy <322.salmo@gmail.com>
2025-10-30 18:12:03 +0000

    Link datapack

--- server_scripts/immersive_geology/tfc_ig_melting_test.js
+++
@@ -1,9 +0,0 @@
-// kubejs/server_scripts/tfc_ig_melting_test.js
-ServerEvents.recipes(event => {
-  event.custom({
-    type: "tfc:heating",
-    ingredient: { item: "immersivegeology:crushed_ore_acanthite" },
-    result_fluid: { fluid: "gtceu:silver", amount: 90 },
-    temperature: 480
-  }).id("kubejs:tfc/heating/ig_acanthite_to_silver");
-});
