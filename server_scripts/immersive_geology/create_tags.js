0aaa79c723cbbf8b9228173d08f5a73e8a043975
calmezyy <322.salmo@gmail.com>
2025-11-10 02:31:37 +0000

    added tags for using gt mats for ig recips

--- server_scripts/immersive_geology/create_tags.js
+++ server_scripts/immersive_geology/create_tags.js
@@ -1,9 +1,249 @@
 // kubejs/server_scripts/create_tags.js
 
 ServerEvents.tags('item', function (event) {
-  // This will create immersivegeology:powder/bauxite if it doesn't exist
-  event.add('immersivegeology:powder/bauxite', [
+  // This will create forge:powder/bauxite if it doesn't exist
+  event.add('forge:powders/bauxite', [
     'immersivegeology:powder_bauxite',
     'gtceu:bauxite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powder/scheelite if it doesn't exist
+  event.add('forge:powders/scheelite', [
+    'immersivegeology:powder_scheelite',
+    'gtceu:scheelite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/molybdenite if it doesn't exist
+  event.add('forge:crushed_ore/molybdenite', [
+    'immersivegeology:forge:crushed_ore_molybdenite',
+    'gtceu:purified_molybdenite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/chalcocite if it doesn't exist
+  event.add('forge:crushed_ore/chalcocite', [
+    'immersivegeology:forge:crushed_ore_chalcocite',
+    'gtceu:purified_chalcocite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/chalcocite if it doesn't exist
+  event.add('forge:powders/chalcocite', [
+    'immersivegeology:forge:powder_chalcocite',
+    'gtceu:chalcocite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/ilmenite if it doesn't exist
+  event.add('forge:crushed_ore/ilmenite', [
+    'immersivegeology:crushed_ore_ilmenite',
+    'gtceu:purified_ilmenite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/copper if it doesn't exist
+  event.add('forge:crushed_ore/copper', [
+    'immersivegeology:crushed_ore_copper',
+    'create:crushed_raw_copper'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/galena if it doesn't exist
+  event.add('forge:crushed_ore/galena', [
+    'immersivegeology:crushed_ore_galena',
+    'gtceu:purified_galena_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/millerite if it doesn't exist
+  event.add('forge:crushed_ore/millerite', [
+    'immersivegeology:crushed_ore_millerite',
+    'gtceu:purified_galena_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/gold if it doesn't exist
+  event.add('forge:crushed_ore/gold', [
+    'immersivegeology:crushed_ore_gold',
+    'create:crushed_ore_gold'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/platinum if it doesn't exist
+  event.add('forge:crushed_ore/platinum', [
+    'immersivegeology:crushed_ore_platinum',
+    'gtceu:purified_platinum_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:grits/platinum if it doesn't exist
+  event.add('forge:grits/platinum', [
+    'immersivegeology:grit_platinum',
+    'gtceu:platinum_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/silver if it doesn't exist
+  event.add('forge:crushed_ore/silver', [
+    'immersivegeology:crushed_ore_silver',
+    'create:crushed_raw_silver'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/apatite if it doesn't exist
+  event.add('forge:powders/apatite', [
+    'immersivegeology:powder_appatite',
+    'gtceu:apatite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/cassiterite if it doesn't exist
+  event.add('forge:crushed_ore/cassiterite', [
+    'immersivegeology:crushed_ore_cassiterite',
+    'gtceu:purified_cassiterite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/cassiterite if it doesn't exist
+  event.add('forge:powders/cassiterite', [
+    'immersivegeology:powder_cassiterite',
+    'gtceu:cassiterite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/chalcopyrite if it doesn't exist
+  event.add('forge:crushed_ore/chalcopyrite', [
+    'immersivegeology:crushed_ore_chalcopyrite',
+    'gtceu:purified_chalcopyrite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/chromite if it doesn't exist
+  event.add('forge:powders/chromite', [
+    'immersivegeology:powder_chromite',
+    'gtceu:chromite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/cobaltite if it doesn't exist
+  event.add('forge:crushed_ore/cobaltite', [
+    'immersivegeology:crushed_ore_cobaltite',
+    'gtceu:purified_cobaltite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/galena if it doesn't exist
+  event.add('forge:powders/galena', [
+    'immersivegeology:powder_galena',
+    'gtceu:galena_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/gypsum if it doesn't exist
+  event.add('forge:crushed_ore/gypsum', [
+    'immersivegeology:crushed_ore_gypsum',
+    'gtceu:purified_gypsum_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/hematite if it doesn't exist
+  event.add('forge:crushed_ore/hematite', [
+    'immersivegeology:crushed_ore_hematite',
+    'gtceu:purified_hematite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/hematite if it doesn't exist
+  event.add('forge:powders/hematite', [
+    'immersivegeology:powder_hematite',
+    'gtceu:hematite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/monazite if it doesn't exist
+  event.add('forge:powders/monazite', [
+    'immersivegeology:powder_monazite',
+    'gtceu:monazite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/pyrite if it doesn't exist
+  event.add('forge:crushed_ore/pyrite', [
+    'immersivegeology:crushed_ore_pyrite',
+    'gtceu:purified_pyrite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/pyrite if it doesn't exist
+  event.add('forge:powders/pyrite', [
+    'immersivegeology:powder_pyrite',
+    'gtceu:pyrite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/pyrolusite if it doesn't exist
+  event.add('forge:crushed_ore/pyrolusite', [
+    'immersivegeology:crushed_ore_pyrolusite',
+    'gtceu:purified_pyrolusite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/pyrolusite if it doesn't exist
+  event.add('forge:powders/pyrolusite', [
+    'immersivegeology:powder_pyrolusite',
+    'gtceu:pyrolusite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:crushed_ore/sphalerite if it doesn't exist
+  event.add('forge:crushed_ore/sphalerite', [
+    'immersivegeology:crushed_ore_sphalerite',
+    'gtceu:purified_sphalerite_ore'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/sphalerite if it doesn't exist
+  event.add('forge:powders/sphalerite', [
+    'immersivegeology:powder_sphalerite',
+    'gtceu:sphalerite_dust'
+  ]);
+});
+
+ServerEvents.tags('item', function (event) {
+  // This will create forge:powders/uraninite if it doesn't exist
+  event.add('forge:powders/uraninite', [
+    'immersivegeology:powder_uraninite',
+    'gtceu:uraninite_dust'
   ]);
 });
